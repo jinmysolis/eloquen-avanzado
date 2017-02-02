bef292bd66d50a8193c6dced8af46e9da15ddb39
unknown <solisjinmy@gmail.com>
2017-02-01 20:03:56 -0430

    softdelete, borradomultiple

---
+++ app/Book.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace AdvancedELOQUENT;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Book extends Model
+{
+   use SoftDeletes;
+}
---
+++ app/Http/routes.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Http\Request;
+
+
+
+Route::get('/', function () {
+    $books= AdvancedELOQUENT\Book::get();
+    return view('destroy', compact('books'));
+});
+
+Route::delete('destroy', function (Request $request){
+    $ids= $request->get('ids');
+    if(count($ids)){
+     AdvancedELOQUENT\Book::destroy($ids);
+	}
+        return back();
+});
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| This route group applies the "web" middleware group to every route
+| it contains. The "web" middleware group is defined in your HTTP
+| kernel and includes session state, CSRF protection, and more.
+|
+*/
+
+Route::group(['middleware' => ['web']], function () {
+    //
+});
