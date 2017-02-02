ed2afcab4ac738f7edd9a243c128d5e7c8d68321
unknown <solisjinmy@gmail.com>
2017-02-01 20:46:29 -0430

    layout

--- resources/views/destroy.blade.php
+++ resources/views/destroy.blade.php
@@ -1,31 +1,34 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Eliminar varios items, Destroy</title>
-    </head>
-
-    <body>
+@extends('layout')
         
-        <p>
+  @section('content')
+
+
+  <p class="btn btn-block btn-info">
             {{ count($books) }}
             Registros
-        </p>
+        </p><br>
         
-        
+        <div class="text-justify">
         <form action="{{url('destroy')}}" method="post">
             {!! csrf_field() !!}
             {!! method_field('delete') !!}
             
              @foreach($books as $book)
-           <input type="checkbox" name="ids[]" value="{{ $book->id }}">{{ $book->title }}<br>
+           <input  type="radio" name="ids[]" value="{{ $book->id }}">---{{ $book->title }}<br><br>
              @endforeach
              
-           <input type="submit" value="Enviar">
+             
+        
+             <br>
+             <input type="submit" value="Enviar" class="btn btn-info">
              
         </form>
+        </div>
         
-        
-        
-        
-    </body>
-</html>
+       
+         
+   
+
+
+
+@endsection
